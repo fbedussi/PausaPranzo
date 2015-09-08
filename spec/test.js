4834d6aa29cd9444ae2388adcdfecaea78b76ebe
Giorgio Aquino <gigetto@gmail.com>
2015-09-08 21:52:24 +0200

    Menu API tests

--- spec/test.js
+++ spec/test.js
@@ -1,8 +1,9 @@
 var request = require("request");
-
+var moment = require('moment');
+var _ = require('lodash');
 var base_url = "http://localhost:8080/"
 
-describe("Hello World Server", function() {
+describe("Homepage", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
@@ -13,13 +14,59 @@ describe("Hello World Server", function() {
   });
 });
 
-describe("Hello World Server", function() {
+describe("Menu API", function() {
   describe("GET /menu", function() {
     it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
+      request.get(base_url + 'menu', function(error, response, body) {
+
+        var today = moment().format('YYYY_MM_DD');
+
         expect(response.statusCode).toBe(200);
+
+        var data = JSON.parse(body);
+
+        expect(typeof data).toBe('object');
+
+        expect(data.length).toBeGreaterThan(0);
+
+        _.each(data, function (dish) {
+
+          expect(dish.date).toBe(today);
+
+        });
+
         done();
       });
     });
   });
+
+  describe("GET /menu/:date", function() {
+    it("returns status code 200", function(done) {
+
+      var d = moment(),
+          today = d.format('YYYY_MM_DD'),
+          yesterday = d.subtract(1, 'day').format('YYYY_MM_DD');
+
+      request.get(base_url + 'menu/' + yesterday, function(error, response, body) {
+
+        expect(response.statusCode).toBe(200);
+
+        var data = JSON.parse(body);
+
+        expect(typeof data).toBe('object');
+
+        expect(data.length).toBeGreaterThan(0);
+
+        _.each(data, function (dish) {
+
+          expect(dish.date).toBe(yesterday);
+          expect(dish.date).not.toBe(today);
+
+        });
+
+        done();
+      });
+    });
+  });
+
 });
